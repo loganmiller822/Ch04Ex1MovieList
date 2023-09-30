33760e0835d304e65691af6b2eb967c81960644b
Logan Miller <logan.miller@southeasttech.edu>
2023-09-30 16:09:12 -0500

    Added movie controller and created views to edit and delete entries.

--- MovieList/Controllers/HomeController.cs
+++ MovieList/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 using MovieList.Models;
 
 namespace MovieList.Controllers
--- MovieList/Models/Movie.cs
+++ MovieList/Models/Movie.cs
@@ -11,7 +11,7 @@ namespace MovieList.Models
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Please enter a year.")]
-        [Range(1989, 2999, ErrorMessage = "Year must be after 1989.")]
+        [Range(1889, 2999, ErrorMessage = "Year must be after 1889.")]
         public int? Year { get; set; }
 
         [Required(ErrorMessage = "Please enter a rating.")]
