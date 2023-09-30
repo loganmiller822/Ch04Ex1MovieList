033835bfdc17a93d336b70afe40b20392751f0c6
Logan Miller <logan.miller@southeasttech.edu>
2023-09-30 15:18:09 -0500

    Modified home controller and added index view.

--- MovieList/Controllers/HomeController.cs
+++ MovieList/Controllers/HomeController.cs
@@ -6,9 +6,14 @@ namespace MovieList.Controllers
 {
     public class HomeController : Controller
     {
+        private MovieContext context {  get; set; }
+        public HomeController(MovieContext ctx) {
+                context = ctx;
+        }
         public IActionResult Index()
         {
-            return View();
+            var movies = context.Movies.OrderBy(m => m.Name).ToList();
+            return View(movies);
         }
     }
 }
